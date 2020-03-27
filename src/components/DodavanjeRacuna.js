429606bc5040361e926dfcf805c5daf7ac17e2c3
ahajradino1 <ahajradino1@etf.unsa.ba>
2020-03-27 01:25:22 +0100

    changed app language

--- src/components/DodavanjeRacuna.js
+++ src/components/DodavanjeRacuna.js
@@ -14,30 +14,27 @@ function DodavanjeRacuna() {
 
   return (
     <div>
-      <h1>Dodaj novi račun</h1>
+      <h1>Add new bank account</h1>
       <div className="container">
         <Form name="add-account" className="accForm" onFinish={onFinish}>
-          <Form.Item label="Broj kartice" colon="false">
+          <Form.Item label="Card number" colon="false">
             <Form.Item
               name="brojKartice"
-              rules={[
-                { required: true, message: "Broj kartice je neophodan" },
-                { isCardValid }
-              ]}
+              rules={[{ required: true, message: "Card number is required" }]}
             >
-              <Input style={{ width: 300 }} placeholder="Unesi broj kartice" />
+              <Input style={{ width: 300 }} placeholder="Enter card number" />
             </Form.Item>
           </Form.Item>
 
-          <Form.Item colon="false" label="Datum isteka">
+          <Form.Item colon="false" label="Expiration date">
             <Form.Item
               name="datumIsteka"
-              rules={[{ required: true, message: "Datum isteka je neophodan" }]}
+              rules={[{ required: true, message: "Date is required" }]}
             >
               <DatePicker
                 picker="month"
                 style={{ width: 150 }}
-                placeholder="Odaberi datum"
+                placeholder="Pick a date"
                 bordered={true}
               />
             </Form.Item>
@@ -46,24 +43,29 @@ function DodavanjeRacuna() {
           <Form.Item colon="false" label="CVC">
             <Form.Item
               name="cvc"
-              rules={[{ required: true, message: "CVC je neophodan" }]}
+              rules={[{ required: true, message: "CVC is required" }]}
             >
-              <Input style={{ width: 150 }} placeholder="Unesi CVC" />
+              <Input style={{ width: 150 }} placeholder="Enter CVC" />
             </Form.Item>
           </Form.Item>
 
-          <Form.Item colon="false" label="Vlasnik računa: ">
+          <Form.Item colon="false" label="Cardholder name: ">
             <Form.Item
               name="imeVlasnika"
-              rules={[{ required: true, message: "Ime vlasnika je neophodno" }]}
+              rules={[
+                { required: true, message: "Cardholder name is required" }
+              ]}
             >
-              <Input style={{ width: 300 }} placeholder="Unesi ime vlasnika" />
+              <Input
+                style={{ width: 300 }}
+                placeholder="Enter cardholder name"
+              />
             </Form.Item>
           </Form.Item>
 
           <Form.Item>
             <Button className="dodajRacun" type="primary" htmlType="submit">
-              Dodaj račun
+              Add account
             </Button>
           </Form.Item>
         </Form>
