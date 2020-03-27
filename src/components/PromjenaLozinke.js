1df4afb994395e05cb04a4665a27d723861e23ed
epita1 <epita1@etf.unsa.ba>
2020-03-27 21:36:04 +0100

    Zavrsen okvir komponente, dodati funkcionalnost i css

--- src/components/PromjenaLozinke.js
+++ src/components/PromjenaLozinke.js
@@ -1,13 +1,40 @@
 import React from "react";
 
 
-// rad na ovoj komponenti
 function PromjenaLozinke() {
   return (
     <div>
-      <p>PromjenaLozinke</p>
+      <header style = {headerStyle}>Change password</header>
+      <form style={formStyle}>
+        <label style={column}>
+          Input current password:
+          <input type="password" name="currPass" />
+        </label>
+        <label style={column}>
+          New password:
+          <input type="password" name="newPass" />
+        </label>
+        <label style={column}>
+          Retype new password:
+          <input type="password" name="retypePass" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
+}
+
+const headerStyle = {
+  fontSize: 'large',
+  fontWeight: 'bold' 
+}
+
+const formStyle = {
+  display: 'flex',
+}
+
+const column = {
+  flex: '50%'
 }
 
 export default PromjenaLozinke;
