44539153f6ed65348b8704162f266799d96d8a76
ahajradino1 <ahajradino1@etf.unsa.ba>
2020-03-27 01:52:29 +0100

    updated navigation and changes app language

---
+++ src/components/TransakcijeMerchant.js
@@ -0,0 +1,10 @@
+import React from "react";
+function TransakcijeMerchant() {
+  return (
+    <div>
+      <h1>TransakcijeMerchant</h1>
+    </div>
+  );
+}
+
+export default TransakcijeMerchant;
---
+++ src/components/TransakcijeProizvod.js
@@ -0,0 +1,10 @@
+import React from "react";
+function TransakcijeProizvod() {
+  return (
+    <div>
+      <h1>TransakcijeProizvod</h1>
+    </div>
+  );
+}
+
+export default TransakcijeProizvod;
