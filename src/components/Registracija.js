c9e45fa04188a561a2e7670a80cd594f63d1e027
kkadusic <kkadusic1@etf.unsa.ba>
2020-03-28 17:51:28 +0100

    Added user registration #9

--- src/components/Registracija.js
+++ src/components/Registracija.js
@@ -1,11 +1,114 @@
 import React from "react";
+import {Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete} from 'antd';
+import {UserOutlined, MailOutlined, LockOutlined} from "@ant-design/icons";
 
-function Registracija() {
-  return (
-    <div>
-      <h1>Registracija</h1>
-    </div>
-  );
-}
+const FormItem = Form.Item;
+
+const Registracija = () => {
+
+    const onFinish = values => {
+        console.log('Received values of form: ', values);
+    };
+
+    return (
+        <Form onFinish={onFinish}>
+
+            <Form.Item name="firstName"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Please input your first name!',
+                           },
+                       ]}
+            >
+                <Input prefix={<UserOutlined/>}
+                       placeholder="First name"
+                />
+            </Form.Item>
+
+
+            <Form.Item name="lastName"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Please input your last name!',
+                           },
+                       ]}
+            >
+                <Input prefix={<UserOutlined/>}
+                       placeholder="Last name"
+                />
+            </Form.Item>
+
+
+            <Form.Item name="username"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Please input your username!',
+                           },
+                       ]}
+            >
+                <Input prefix={<UserOutlined/>}
+                       placeholder="Username"
+                />
+            </Form.Item>
+
+
+            <Form.Item name="email"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Please input your E-mail!',
+                           },
+                       ]}
+            >
+                <Input prefix={<MailOutlined/>}
+                       type="email"
+                       placeholder="E-mail"
+                />
+            </Form.Item>
+
+
+            <Form.Item name="password"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Please input your password!',
+                           },
+                       ]}
+            >
+                <Input prefix={<LockOutlined/>}
+                       type="password"
+                       placeholder="Password"
+                />
+            </Form.Item>
+
+
+            <Form.Item name="confirmPassword"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Please confirm your password!',
+                           },
+                       ]}
+            >
+                <Input prefix={<LockOutlined/>}
+                       type="password"
+                       placeholder="Confirm password"
+                />
+            </Form.Item>
+
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="register-button">
+                    Register
+                </Button>
+            </Form.Item>
+
+
+        </Form>
+    );
+};
 
 export default Registracija;
