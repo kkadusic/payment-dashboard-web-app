459a0e06602fab21556a597af3ab0a1bbfff024d
epita1 <epita1@etf.unsa.ba>
2020-03-29 19:35:29 +0200

    Prepravljen kostur forme, dodan paket semantic-ui-react

--- src/components/PromjenaLozinke.js
+++ src/components/PromjenaLozinke.js
@@ -1,36 +1,67 @@
 import React from "react";
+import {Form, Input, Button} from 'antd';
+import {LockOutlined} from "@ant-design/icons";
+import { Label } from 'semantic-ui-react';
 import "../css/PromjenaLozinke.css";
 
 
 function PromjenaLozinke() {
+
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
     <React.Fragment>
-      <header style = {headerStyle}>Change password</header>
-      <form>
-        <label>
-          Input current password:
-          <input type="password" name="currPass" />
-        </label>
-        <label>
-          New password:
-          <input type="password" name="newPass" />
-        </label>
+      <div className="title">
+          <h1>Change password</h1>
+        </div>
         <br></br>
-        <label>
-          Retype new password:
-          <input type="password" name="retypePass" />
-        </label>
-        <br></br>
-        <input type="submit" value="Submit" />
-      </form>
+        <Form onFinish={onFinish}>
+          <Label>
+            Input your old password:
+          </Label>
+            <Form.Item name="Oldassword"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Please input your old password!',
+                           },
+                       ]}
+            >
+                <Input prefix={<LockOutlined/>}
+                       type="password"
+                       placeholder="Old password"
+                />
+            </Form.Item>
+
+            <Label>
+              Input your new password:
+            </Label>
+            <Form.Item name="newPassword"
+                       rules={[
+                           {
+                               required: true,
+                               message: 'Please input your new password!',
+                           },
+                       ]}
+            >
+                <Input prefix={<LockOutlined/>}
+                       type="password"
+                       placeholder="New password"
+                />
+            </Form.Item>
+
+
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="submit-button">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
     </React.Fragment>
   );
-}
-
-const headerStyle = {
-  fontSize: 'large',
-  fontWeight: 'bold',
-  padding: '15px' 
 }
 
 export default PromjenaLozinke;
