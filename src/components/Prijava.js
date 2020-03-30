4948ead22898530be16027064fcb94c7185446c4
Amila Fejzic <amila.fejzic@foreo.com>
2020-03-30 15:59:29 +0200

    API Error handling

--- src/components/Prijava.js
+++ src/components/Prijava.js
@@ -2,73 +2,95 @@ import React, { useState } from "react";
 import "../css/Prijava.css";
 import { Form, Input, Button, Checkbox, Alert } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { saveUserToken, saveUserData } from "../utilities/Common";
 import axios from "axios";
 
-const Prijava = props => {
-  const onFinish = values => {
-    const config = {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    };
-    // Send login request
-    axios
-      .post(
-        "https://payment-server-si.herokuapp.com/api/auth/signin",
-        {
-          usernameOrEmail: values.email,
-          password: values.password
-        },
-        config
-      )
-      .then(response => {
-        // Save user token to local storage
-        saveUserToken(response.data.accessToken, response.data.tokenType);
-        return response.data;
-      })
-      .then(data => {
-        const token = data.tokenType + " " + data.accessToken;
-        // Send get user data request
-        return axios.get(
-          "https://payment-server-si.herokuapp.com/api/auth/user/me",
-          {
-            headers: {
-              Authorization: token
-            }
-          }
-        );
-      })
-      .then(response => {
-        //Create user object
-        let user = {
-          firstName: response.data.firstName,
-          lastName: response.data.lastName,
-          username: response.data.userName,
-          email: response.data.email
-        };
-        // Save user object to storage
-        saveUserData(user);
-        console.log("Login successful");
-        props.history.push("/pocetna");
-      })
-      .catch(error => {
-        // if (error.response.data.path == "/api/auth/signin") {
-        // }
-        if (error.response == null) {
-          console.log("No internet");
-          return;
-        }
-        if (error.response.status === 401)
-          console.log("Wrong username or password/Auth error", error.response);
-        else console.log(error.response.data.message);
-      });
+function Prijava() {
+	
+	const history = useHistory();
+	const [warning, setWarning] = useState(false)
+	const [error, setError] = useState("")
+	// const [token, setToken] = useState({
+	// 	token: ""
+	// })
+	// const [user, setUser] = useState({
+	// 	user: {}
+	// })
+	const hideWarning = () => {
+        setWarning(false);
+	}	
+	const onFinish = values => {
+	const config = {
+		headers: {
+			"Content-Type": "application/json"
+		}
+	};
+		// Send login request
+	axios
+	.post(
+		"https://payment-server-si.herokuapp.com/api/auth/signin",
+		{
+			usernameOrEmail: values.email,
+			password: values.password
+		},
+		config
+	)
+	.then(response => {
+		// Save user token to local storage
+		saveUserToken(response.data.accessToken, response.data.tokenType);
+		// setToken({token: response.data.accessToken})
+		return response.data;
+	})
+	.then(data => {
+		const accessToken = data.tokenType + " " + data.accessToken;
+		// Send get user data request
+		return axios.get(
+			"https://payment-server-si.herokuapp.com/api/auth/user/me",
+			{
+				headers: {
+					Authorization: accessToken
+				}
+			}
+		);
+	})
+	.then(response => {
+		//Create user object
+		let user = {
+			firstName: response.data.firstName,
+			lastName: response.data.lastName,
+			username: response.data.userName,
+			email: response.data.email
+		};
+		// Save user object to storage
+		saveUserData(user);
+		// setUser(user)
+		console.log("Login successful");
+  })
+  .then(() => {
+    history.push("/pocetna");
+  })
+	.catch(error => {
+		// if (error.response.data.path == "/api/auth/signin") {
+		// }
+		setWarning(true)
+		if (error.response == null) {
+			console.log("No internet");
+			setError("No internet")
+			return;
+		}
+		if (error.response.status === 401){
+			setError("Wrong username or password!")
+			console.log("Wrong username or password/Auth error", error.response);
+
+		}
+		else console.log(error.response.data.message);
+	});
   };
 
-  return (
-    <div>
-      <div className="split left">
+  	return (
+    	<div>
+      		<div className="split left">
         <div>
           <h1 style={{ color: "white", fontStyle: "italic", fontSize: "60px" }}>
             Payment
@@ -99,6 +121,7 @@ const Prijava = props => {
               <Input
                 prefix={<UserOutlined />}
                 placeholder="Username or Email"
+                onChange={hideWarning}
               />
             </Form.Item>
 
@@ -115,8 +138,18 @@ const Prijava = props => {
                 prefix={<LockOutlined />}
                 type="password"
                 placeholder="Password"
+                onChange={hideWarning}
               />
             </Form.Item>
+            { warning ? 
+              <Alert
+                message="Error"
+                description={error}
+                type="error"
+                style={{ marginBottom: '10px'}}
+                showIcon
+                /> : null
+            }
             <Form.Item>
               <Form.Item name="remember" valuePropName="checked" noStyle>
                 <Checkbox className="login-remember-checkbox">
