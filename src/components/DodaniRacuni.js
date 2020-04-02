165f2cbd225da41437a5ef112c54e0cfc076fc9d
nerminKrdzic <nermin.krdzic@pragmatio.com>
2020-04-03 00:32:41 +0200

    Added reload button for accounts review.

--- src/components/DodaniRacuni.js
+++ src/components/DodaniRacuni.js
@@ -1,37 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { Spin, Collapse,  } from "antd";
+import {Collapse, Button, Tooltip} from "antd";
 import AccountComponent from "./accountListComponent/accountComponent"
-import {CaretRightOutlined} from "@ant-design/icons";
+import {CaretRightOutlined, ReloadOutlined} from "@ant-design/icons";
 import axios from "axios"
 
 import "./accountListComponent/accountComponent.css"
 import {getToken} from "../utilities/Common";
+import LoadingOutlined from "@ant-design/icons/lib/icons/LoadingOutlined";
 
 const { Panel } = Collapse;
 
+const loadData = (setAccounts) => {
+    axios.get(
+        "https://payment-server-si.herokuapp.com/api/accounts/all", {
+            headers: {
+                Authorization: "Bearer " + getToken()
+            }
+        }
+    ).then(res => {
+        console.log(res.data);
+        setAccounts({
+            data: res.data,
+            loading: false
+        });
+    }).catch(err => {
+        console.log(err);
+    });
+};
+
 function DodaniRacuni() {
-  const [accounts, setAccounts] = useState({
-      data: [],
-      loading: true
-  });
+    const [accounts, setAccounts] = useState({
+        data: null,
+        loading: true
+    });
 
-  const loadData = () => {
-      axios.get(
-          "https://payment-server-si.herokuapp.com/api/accounts/all", {
-              headers: {
-                  Authorization: "Bearer " + getToken()
-              }
-          }
-      ).then(res => {
-          setAccounts({
-              data: res.data,
-              loading: false
-          });
-      }).catch(err => {
-          console.log(err);
-      });
 
-  };
 
   const deleteAccount = (id) => {
     axios.delete(
@@ -41,33 +44,47 @@ function DodaniRacuni() {
             }
         }
     ).then(() => {
-        setAccounts({
-            data: [],
-            loading: true
-        });
-        loadData();
+        reload()
     }).catch(err => {
         console.log(err);
     });
   };
 
+  const reload = () => {
+      setAccounts({
+          data: null,
+          loading: true
+      });
+      loadData(setAccounts);
+  };
+
   // componentDidMount
-  useEffect(loadData, []);
+  useEffect(() => {
+      if (accounts.data === null) loadData(setAccounts);
+  }, [setAccounts]);
 
   return (
     <div>
       { (accounts.loading || !accounts.data) ? (
-        <Spin size="large" style={{ "margin": "0" }}/>
+          <Tooltip title={"Loading..."}>
+              <Button type={"primary"} shape={"circle"} className={"reload-button"} icon={<LoadingOutlined />}/>
+          </Tooltip>
       ) : (
-        <Collapse accordion bordered={false}
-          expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0}/>}
-          className="site-collapse-custom-collapse">
-          {accounts.data.map(element =>
-              <Panel header={element.bankName} key={element.id} className="site-collapse-custom-panel" forceRender={true}>
-                <AccountComponent account = {element} deleteAccount = {deleteAccount}/>
-              </Panel>
-          )}
-        </Collapse>
+          <div>
+              <Tooltip title={"Reload"}>
+                  <Button type="primary" shape="circle" className={"reload-button"} onClick={reload}  icon={<ReloadOutlined />} />
+              </Tooltip>
+              <Collapse accordion bordered={false}
+                        expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0}/>}
+                        className="site-collapse-custom-collapse">
+                  {accounts.data.map(element =>
+                      <Panel header={element.bankName} key={element.id} className="site-collapse-custom-panel" forceRender={true}>
+                          <AccountComponent account = {element} deleteAccount = {deleteAccount}/>
+                      </Panel>
+                  )}
+              </Collapse>
+          </div>
+
 
       )}
     </div>
