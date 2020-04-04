7f4f306104ed390d4f1f8a1870f33694ca8ea59b
ahajradino1 <ahajradino1@etf.unsa.ba>
2020-04-04 20:05:30 +0200

    read all transactions

--- src/components/transactions/PregledTransakcija.js
+++ src/components/transactions/PregledTransakcija.js
@@ -4,87 +4,64 @@ import "antd/dist/antd.css";
 import { Table, Input, Button } from "antd";
 import Highlighter from "react-highlight-words";
 import { SearchOutlined } from "@ant-design/icons";
-
-const data = [
-  {
-    key: "1",
-    cardNumber: "1111111111111112",
-    merchant: "Bingo",
-    date: "01/04/2020",
-    price: 40
-  },
-  {
-    key: "2",
-    cardNumber: "1111111111111112",
-    merchant: "Bingo",
-    date: "01/03/2020",
-    price: 40
-  },
-  {
-    key: "3",
-    cardNumber: "1111111111111112",
-    merchant: "Bingo",
-    date: "29/02/2020",
-    price: 40
-  },
-  {
-    key: "4",
-    cardNumber: "1111111111111112",
-    merchant: "Bingo",
-    date: "15/02/2020",
-    price: 540
-  },
-  {
-    key: "5",
-    cardNumber: "1111111111111117",
-    merchant: "Bingo",
-    date: "02/04/2020",
-    price: 1230
-  },
-  {
-    key: "6",
-    cardNumber: "1111111111111112",
-    merchant: "Mercator",
-    date: "31/03/2020",
-    price: 1730
-  },
-  {
-    key: "7",
-    cardNumber: "1111111111111115",
-    merchant: "Cue",
-    date: "31/03/2019",
-    price: 1730
-  },
-  {
-    key: "8",
-    cardNumber: "1111111111111112",
-    merchant: "Yamm",
-    date: "26/11/2019",
-    price: 20
-  }
-];
+import { getToken } from "../../utilities/Common";
+import axios from "axios";
+import uuid from "react-uuid";
 
 class PregledTransakcija extends Component {
   state = {
     searchText: "",
-    searchedColumn: ""
+    searchedColumn: "",
+    data: [],
+    key: 0,
   };
 
-  getColumnSearchProps = dataIndex => ({
+  componentWillMount() {
+    this.getTransactions();
+  }
+
+  getTransactions() {
+    axios
+      .get("https://payment-server-si.herokuapp.com/api/transactions/all", {
+        headers: { Authorization: "Bearer " + getToken() },
+      })
+      .then((response) => {
+        const transactions = [];
+        response.data.forEach((transaction) => {
+          transactions.push({
+            key: ++this.state.key,
+            cardNumber: transaction.cardNumber,
+            merchantName: transaction.merchantName,
+            totalPrice: transaction.totalPrice,
+            date: transaction.date.substr(0, 10),
+          });
+        });
+        this.setState({ data: transactions }, () => {
+          //   this.state.data.forEach((element) => {
+          //     //   element["key"] = uuid();
+          //     element["key"] = ++this.state.key;
+          //   });
+          console.log(this.state.data);
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  getColumnSearchProps = (dataIndex) => ({
     filterDropdown: ({
       setSelectedKeys,
       selectedKeys,
       confirm,
-      clearFilters
+      clearFilters,
     }) => (
       <div style={{ padding: 8 }}>
         <Input
-          ref={node => {
+          ref={(node) => {
             this.searchInput = node;
           }}
           placeholder={`Search ${dataIndex}`}
           value={selectedKeys[0]}
-          onChange={e =>
+          onChange={(e) =>
             setSelectedKeys(e.target.value ? [e.target.value] : [])
           }
           onPressEnter={() =>
@@ -110,20 +87,17 @@ class PregledTransakcija extends Component {
         </Button>
       </div>
     ),
-    filterIcon: filtered => (
+    filterIcon: (filtered) => (
       <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
     ),
     onFilter: (value, record) =>
-      record[dataIndex]
-        .toString()
-        .toLowerCase()
-        .includes(value.toLowerCase()),
-    onFilterDropdownVisibleChange: visible => {
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible) => {
       if (visible) {
         setTimeout(() => this.searchInput.select());
       }
     },
-    render: text =>
+    render: (text) =>
       this.state.searchedColumn === dataIndex ? (
         <Highlighter
           highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
@@ -133,18 +107,18 @@ class PregledTransakcija extends Component {
         />
       ) : (
         text
-      )
+      ),
   });
 
   handleSearch = (selectedKeys, confirm, dataIndex) => {
     confirm();
     this.setState({
       searchText: selectedKeys[0],
-      searchedColumn: dataIndex
+      searchedColumn: dataIndex,
     });
   };
 
-  handleReset = clearFilters => {
+  handleReset = (clearFilters) => {
     clearFilters();
     this.setState({ searchText: "" });
   };
@@ -157,15 +131,15 @@ class PregledTransakcija extends Component {
         key: "cardNumber",
         width: "30%",
         sorter: (a, b) => a.cardNumber - b.cardNumber,
-        ...this.getColumnSearchProps("cardNumber")
+        ...this.getColumnSearchProps("cardNumber"),
       },
       {
         title: "Merchant",
-        dataIndex: "merchant",
-        key: "merchant",
+        dataIndex: "merchantName",
+        key: "merchantName",
         width: "20%",
-        sorter: (a, b) => a.merchant < b.merchant,
-        ...this.getColumnSearchProps("merchant")
+        sorter: (a, b) => a.merchantName < b.merchantName,
+        ...this.getColumnSearchProps("merchantName"),
       },
       {
         title: "Date",
@@ -174,16 +148,16 @@ class PregledTransakcija extends Component {
         filters: [
           {
             text: "24 hours",
-            value: "24h"
+            value: "24h",
           },
           {
             text: "Last month",
-            value: "month"
+            value: "month",
           },
           {
             text: "Last year",
-            value: "year"
-          }
+            value: "year",
+          },
         ],
         onFilter: (value, record) => {
           const today = new Date();
@@ -204,9 +178,9 @@ class PregledTransakcija extends Component {
             today.getDate()
           );
 
-          const day = parseInt(record.date.substr(0, 2));
-          const month = parseInt(record.date.substr(3, 5)) - 1;
-          const year = parseInt(record.date.substr(6, 10));
+          const day = parseInt(record.date.substr(8, 10));
+          const month = parseInt(record.date.substr(5, 7)) - 1;
+          const year = parseInt(record.date.substr(0, 4));
           const date = new Date(year, month, day);
 
           if (value === "24h") {
@@ -217,17 +191,17 @@ class PregledTransakcija extends Component {
             );
           } else if (value == "month") return monthAgo <= date && date <= today;
           return yearAgo <= date && date <= today;
-        }
+        },
       },
       {
-        title: "Price",
-        dataIndex: "price",
-        key: "price",
-        sorter: (a, b) => a.price - b.price,
-        ...this.getColumnSearchProps("price")
-      }
+        title: "totalPrice",
+        dataIndex: "totalPrice",
+        key: "totalPrice",
+        sorter: (a, b) => a.totalPrice - b.totalPrice,
+        ...this.getColumnSearchProps("totalPrice"),
+      },
     ];
-    return <Table columns={columns} dataSource={data} />;
+    return <Table columns={columns} dataSource={this.state.data} />;
   }
 }
 
