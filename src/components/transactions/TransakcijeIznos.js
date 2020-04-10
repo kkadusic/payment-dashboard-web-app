2f69e90e5b9d6ae14a56cd8e98b283834e511a34
kkadusic <kkadusic1@etf.unsa.ba>
2020-04-10 19:08:10 +0200

    Add radar chart for merchant-price relation #43

--- src/components/transactions/TransakcijeIznos.js
+++ src/components/transactions/TransakcijeIznos.js
@@ -1,13 +1,68 @@
-import React from "react";
-function TransakcijeIznos() {
-  return (
-    <div>
-      <h1>
-        Prikaz merchanta kod kojih je najviše novca ostavljeno pomoću Radar
-        chart-a (za određeni ili za sve bankovne račune)
-      </h1>
-    </div>
-  );
-}
+import React, {useEffect, useState, useRef} from 'react';
+import Chartjs from 'chart.js';
+import axios from "axios";
+import {getToken} from "../../utilities/Common";
+
+
+const TransakcijeIznos = () => {
+
+    function getAllPricesForMerchants(data){
+        const merchantPrice = [];
+        data.forEach((transaction) => {
+            if (merchantPrice.hasOwnProperty(transaction.merchantName)){
+                merchantPrice[transaction.merchantName] += transaction.totalPrice;
+            }
+            else {
+                merchantPrice[transaction.merchantName] = transaction.totalPrice;
+            }
+        });
+        return merchantPrice;
+    }
+
+    const chartContainer = useRef(null);
+    const [chartInstance, setChartInstance] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get("https://payment-server-si.herokuapp.com/api/transactions/all", {
+                headers: {Authorization: "Bearer " + getToken()}
+            })
+            .then((res) => {
+                console.log(res.data);
+                const merchantPrice = getAllPricesForMerchants(res.data);
+
+                const chartConfig = {
+                    type: 'radar',
+                    data: {
+                        labels: Object.keys(merchantPrice),
+                        datasets: [
+                            {
+                                label: "All bank accounts",
+                                backgroundColor: 'rgba(0, 200, 132, 0.3',
+                                borderColor: 'gray',
+                                data: Object.values(merchantPrice),
+                            }
+                        ]
+                    },
+                    options: {}
+                };
+
+                if (chartContainer && chartContainer.current) {
+                    const newChartInstance = new Chartjs(chartContainer.current, chartConfig);
+                    setChartInstance(newChartInstance);
+                }
+
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [chartContainer]);
+
+    return (
+        <div>
+            <canvas ref={chartContainer}/>
+        </div>
+    );
+};
 
 export default TransakcijeIznos;
