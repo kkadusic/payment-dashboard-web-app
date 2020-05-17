a21152d89329879c2ac2890c7809f090812dd094
ahajradino1 <ahajradino1@etf.unsa.ba>
2020-05-17 17:50:39 +0200

    added test script for selenium

--- SeleniumTests.js
+++ SeleniumTests.js
@@ -1,15 +1,15 @@
 const { Builder, By, Key } = require("selenium-webdriver");
 assert = require("assert");
 
-describe("nesto", () => {
-  it("emailFieldEmpty", () => {
-    let driver = new Builder().forBrowser("chrome").build();
-    driver.get("https://payment-dashboard.herokuapp.com/");
-    driver.findElement(By.id("email")).sendKeys("");
-    driver.findElement(By.id("password")).sendKeys("neki password");
-    driver.findElement(By.tagName("button")).click();
-    const currentUrl = driver.getCurrentUrl();
-    //ovdje je response Promise !!!
+describe("login", function () {
+  this.timeout(30000);
+  it("email field shouldn't be empty", async () => {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://payment-dashboard.herokuapp.com/");
+    await driver.findElement(By.id("email")).sendKeys("");
+    await driver.findElement(By.id("password")).sendKeys("neki password");
+    await driver.findElement(By.tagName("button")).click();
+    const currentUrl = await driver.getCurrentUrl();
     assert.equal(currentUrl, "https://payment-dashboard.herokuapp.com/");
   });
 });
