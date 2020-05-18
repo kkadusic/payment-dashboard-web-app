2008fdc480e6b27a392f467943c6cb76d3c4e5f1
ahajradino1 <ahajradino1@etf.unsa.ba>
2020-05-18 15:23:07 +0200

    editet jest tests

--- src/App.test.js
+++ src/App.test.js
@@ -9,18 +9,33 @@ import Prijava from "./components/Prijava";
 
 Enzyme.configure({ adapter: new Adapter() });
 
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 test("component should render without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<PregledTransakcija />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
 
 test("component should contain table (for transactions)", () => {
-  const transactions = shallow(<PregledTransakcija></PregledTransakcija>);
+  const transactions = shallow(<PregledTransakcija />);
   expect(transactions.find("Table")).toHaveLength(1);
 });
 
 test("registration form should have two password fields", () => {
-  const component = shallow(<RegistrationForm></RegistrationForm>);
+  const component = shallow(<RegistrationForm />);
   expect(component.find('[name="password"]')).toHaveLength(1);
   expect(component.find('[name="confirm"]')).toHaveLength(1);
 });
