c2f599e91bea84a6857ed746d8986b3da8d6c548
ahajradino1 <ahajradino1@etf.unsa.ba>
2020-05-18 03:40:39 +0200

    added more tests

--- SeleniumTests.js
+++ SeleniumTests.js
@@ -52,4 +52,117 @@ describe("login", function () {
           });
       });
   });
+
+  it("right credentials", () => {
+    driver
+      .get("https://payment-dashboard.herokuapp.com/")
+      .then(() => driver.findElement(By.id("email")).sendKeys("ajsa123"))
+      .then(() => driver.findElement(By.id("password")).sendKeys("ajsa123"))
+      .then(() => driver.findElement(By.tagName("button")).click())
+      .then(() => driver.getCurrentUrl())
+      .then((url) => {
+        assert.equal(url, "https://payment-dashboard.herokuapp.com/pocetna");
+        done();
+      });
+  });
+});
+
+describe("user is logged in", function () {
+  this, this.timeout(50000);
+  let driver;
+
+  it("logging out", (done) => {
+    driver = new Builder().forBrowser("chrome").build();
+    driver
+      .get("https://payment-dashboard.herokuapp.com/")
+      .then(() => driver.findElement(By.id("email")).sendKeys("ajsa123"))
+      .then(() => driver.findElement(By.id("password")).sendKeys("ajsa123"))
+      .then(() => driver.findElement(By.tagName("button")).click())
+      .then(() =>
+        driver.wait(
+          until.elementLocated(
+            By.xpath("//div[@id='root']/section/section/aside/div/ul/li[7]/a")
+          ),
+          15000
+        )
+      )
+      .then(() =>
+        driver
+          .findElement(
+            By.xpath("//div[@id='root']/section/section/aside/div/ul/li[7]/a")
+          )
+          .click()
+      )
+      .then(() =>
+        driver.wait(
+          until.elementLocated(By.xpath("(//button[@type='button'])[3]")),
+          15000
+        )
+      )
+      .then(() => {
+        driver.findElement(By.xpath("(//button[@type='button'])[3]")).click();
+      })
+      .then(() => driver.getCurrentUrl())
+      .then((url) => {
+        assert.equal(url, "https://payment-dashboard.herokuapp.com/logout");
+        done();
+      });
+  });
+
+  it("adding bank account", (done) => {
+    driver = new Builder().forBrowser("chrome").build();
+    driver
+      .get("https://payment-dashboard.herokuapp.com/")
+      .then(() => driver.findElement(By.id("email")).sendKeys("ajsa123"))
+      .then(() => driver.findElement(By.id("password")).sendKeys("ajsa123"))
+      .then(() => driver.findElement(By.tagName("button")).click())
+      .then(() =>
+        driver.wait(
+          until.elementLocated(
+            By.xpath(
+              "//div[@id='root']/section/section/aside/div/ul/li[4]/div/span"
+            )
+          ),
+          15000
+        )
+      )
+      .then(() =>
+        driver
+          .findElement(
+            By.xpath(
+              "//div[@id='root']/section/section/aside/div/ul/li[4]/div/span"
+            )
+          )
+          .click()
+      )
+      .then(() =>
+        driver.wait(
+          until.elementLocated(By.xpath("//ul[@id='sub2$Menu']/li[3]/a")),
+          15000
+        )
+      )
+      .then(() => {
+        driver
+          .findElement(By.xpath("//ul[@id='sub2$Menu']/li[3]/a"))
+          .click()
+          .then(() => {
+            driver
+              .findElement(By.css("#add-account_cardNumber"))
+              .sendKeys("123456789123456");
+          })
+          .then(() =>
+            driver
+              .findElement(
+                By.xpath(
+                  "//form[@id='add-account']/div[2]/div[2]/div/div/div/div/div/div/div"
+                )
+              )
+              .sendKeys(new Date(2020, 1, 1))
+          )
+          .then(() =>
+            driver.findElement(By.css("#add-account_cvc")).sendKeys("123")
+          )
+          .then(() => () => driver.findElement(By.tagName("button")).click());
+      });
+  });
 });
